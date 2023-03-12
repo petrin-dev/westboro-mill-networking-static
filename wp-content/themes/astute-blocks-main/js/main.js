113572119e4cabb1bdfabfca92301abaf477aca9
Rob Petrin <robpetrin@gmail.com>
2023-03-12 13:16:27 -0400

    Form Validation Fix

--- wp-content/themes/astute-blocks-main/js/main.js
+++ wp-content/themes/astute-blocks-main/js/main.js
@@ -1,1 +1,19 @@
- 
+const emailRegex = 
+ new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+ const isValidEmail = emailRegex.test("email_address@domain.com")
+
+let emailField = document.querySelector('#your-email')
+if (emailField) {
+    let emailError = document.querySelector('.form-error-email')
+    let submitButton = document.querySelector('#submit-button')
+    emailField.addEventListener('keyup',function(){
+        const isValidEmail = emailRegex.test(emailField.value)
+        if (isValidEmail) {
+            emailError.style.display = 'none'
+            submitButton.disabled = 'false'
+        } else {
+            emailError.style.display = 'inline-block'
+            submitButton.disabled = 'true'
+        }
+    })
+}
